b637f758f064ac2086f2442ce6b1fc7f5ebd9ce0
Jon Jones <jonathajones@paypal.com>
2021-02-10 13:25:23 -0500

    Updating indentation and formatting for package file
    Former-commit-id: 99b06b37ae3f4bfd3efd174bd49eb4f34dc2fcc7

--- Package.swift
+++ Package.swift
@@ -11,14 +11,15 @@ let package = Package(
   products: [
     .library(
       name: "PayPalCheckout",
-      targets: ["PayPalCheckout"])
+      targets: ["PayPalCheckout"]
+    )
   ],
   targets: [
     .binaryTarget(
       name: "PayPalCheckout",
       url: "https://github.com/paypal/paypalcheckout-ios/releases/download/\(packageVersion)/PayPalCheckout.xcframework.zip",
       checksum: "d070ef614643373e57d0a25c99c7dde1317556a8622e7627d797e3daf28f086b"
-      )
+    )
   ],
   swiftLanguageVersions: [.v5]
 )
