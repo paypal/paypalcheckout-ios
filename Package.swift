c0602abbb1c949a38c6f29113f65a5f2bfd61df4
Jon Jones <jonathajones@paypal.com>
2021-03-18 13:33:25 -0500

    Carthage and Package version bump to 0.39.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let packageVersion = "0.38.0"
+let packageVersion = "0.39.0"
 
 let package = Package(
   name: "PayPalCheckout",
@@ -18,7 +18,7 @@ let package = Package(
     .binaryTarget(
       name: "PayPalCheckout",
       url: "https://github.com/paypal/paypalcheckout-ios/releases/download/\(packageVersion)/PayPalCheckout.xcframework.zip",
-      checksum: "290153c2bc017fa519fbcfca8640d8eeeeec27739328a898ba9a31988de39160"
+      checksum: "c8fa4a86330d2183f71837161102b134a282678613c277a29fb726ad7f57e13a"
     )
   ],
   swiftLanguageVersions: [.v5]
