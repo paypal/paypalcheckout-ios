0d1ee6815a277bb92e974fe510d738b20827caa4
Jon Jones <jonathajones@paypal.com>
2021-05-17 12:46:47 -0500

    Update SPM and Carthage version to 0.51.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let packageVersion = "0.46.0"
+let packageVersion = "0.51.0"
 
 let package = Package(
   name: "PayPalCheckout",
@@ -18,7 +18,7 @@ let package = Package(
     .binaryTarget(
       name: "PayPalCheckout",
       url: "https://github.com/paypal/paypalcheckout-ios/releases/download/\(packageVersion)/PayPalCheckout.xcframework.zip",
-      checksum: "7728bfe266ade19ae0a0b50b83f51549612bf1778e1e3596e9ab21e74671d970"
+      checksum: "82f65aa674bd6326bb729f3d36f5329d2e9c9115af0bf6a9a8de5029dd417016"
     )
   ],
   swiftLanguageVersions: [.v5]
