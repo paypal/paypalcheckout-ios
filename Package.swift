7c6750e1316c6a3d656e90497271de68c63219f1
Jon Jones <jonathajones@paypal.com>
2023-08-17 23:01:03 -0500

    Updating to 1.1.0

--- Package.swift
+++ Package.swift
@@ -1,7 +1,7 @@
 // swift-tools-version:5.3
 import PackageDescription
 
-let packageVersion = "1.0.0"
+let packageVersion = "1.1.0"
 
 let package = Package(
   name: "PayPalCheckout",
@@ -18,7 +18,7 @@ let package = Package(
     .binaryTarget(
       name: "PayPalCheckout",
       url: "https://github.com/paypal/paypalcheckout-ios/releases/download/\(packageVersion)/PayPalCheckout.xcframework.zip",
-      checksum: "653381b514b6d5de1a85fe80d0085217a732c9de2da6ea0afe61f98295d1b67d"
+      checksum: "e9e8d7ed4c48de1323bed5be81ddbc4fc82107506a6d23edc55689214f75f7ea"
     )
   ],
   swiftLanguageVersions: [.v5]
